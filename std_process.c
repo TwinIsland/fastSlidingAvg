81eda2da391c9809f5ab673d038a91608bcdf283
Yunfan Hu <yunfanh2@illinois.edu>
2023-10-26 10:55:44 -0500

    add refactor details

--- std_process.c
+++ std_process.c
@@ -4,6 +4,16 @@
 #include <assert.h>
 #include "std_process.h"
 #include <string.h>
+#include <stdbool.h>
+
+char line[128];
+char *token;
+long time;
+double price, volume;
+static int init_step_flag = 1;
+static int queue_count = 0; // Debugging: count queue size
+static double frame_t_price = 0.0;
+static int n_frame_item = 0;
 
 SlidingWindow* sliding_window_create(int window_size, int hop_size) {
     SlidingWindow *sw = (SlidingWindow *)malloc(sizeof(SlidingWindow));
@@ -15,6 +25,7 @@ SlidingWindow* sliding_window_create(int window_size, int hop_size) {
     sw->hop_size = hop_size;
     sw->window_t_price = 0;
     sw->n_window_item = 0;
+    sw->prev_time = 0;
     initializeQueue(sw->frame_t_price_queue, window_size / hop_size);
     initializeQueue(sw->n_frame_item_queue, window_size / hop_size);
     return sw;
@@ -27,12 +38,7 @@ void sliding_window_destroy(SlidingWindow *sw) {
     free(sw);
 }
 
-void process_trade(SlidingWindow *sw, long time, double price, double volume) {
-    static int init_step_flag = 1;
-    static int queue_count = 0; // Debugging: count queue size
-    static double frame_t_price = 0.0;
-    static int n_frame_item = 0;
-
+void InitSlidingWindow(SlidingWindow *sw) {
     price *= volume;
 
     if (init_step_flag) {
@@ -47,15 +53,16 @@ void process_trade(SlidingWindow *sw, long time, double price, double volume) {
             n_frame_item++;
         } else {
             enqueue(sw->frame_t_price_queue, frame_t_price);
+            enqueue(sw->n_frame_item_queue, n_frame_item);
             sw->window_t_price += frame_t_price;
             sw->n_window_item += n_frame_item;
 
             queue_count++; // Debugging
+            printf("%d\n", queue_count);
 
             // append zeros for missing frames
-            for (long i = sw->cur_frame_starttime + sw->hop_size; i < time; i += sw->hop_size) {
-                enqueue(sw->frame_t_price_queue, 0);
-                queue_count++; // Debugging
+            for (long i = sw->prev_time + sw->hop_size; i < time; i += sw->hop_size) {
+                pushZero(sw, true);
                 sw->cur_frame_starttime += sw->hop_size;
             }
 
@@ -64,14 +71,76 @@ void process_trade(SlidingWindow *sw, long time, double price, double volume) {
             frame_t_price = price;
             sw->cur_frame_starttime += sw->hop_size;
         }
+        sw->prev_time = time;
     }
 
     assert(queue_count == sw->window_size / sw->hop_size);
 }
 
+void SlideTheWindow(SlidingWindow *sw) {
+
+    while (fgets(line, sizeof(line), sw->fp)) {
+        token = strtok(line, ",");
+        time = atol(token);
+
+        token = strtok(NULL, ",");
+        price = atof(token);
+
+        token = strtok(NULL, ",");
+        volume = atof(token);
+
+        price *= volume;
+
+        if (time < sw->cur_frame_starttime + sw->hop_size) {
+            frame_t_price += price;
+            n_frame_item++;
+        } else {
+            sw->window_t_price += frame_t_price - dequeue(sw->frame_t_price_queue);
+            sw->n_window_item += n_frame_item - dequeue(sw->n_frame_item_queue);
+
+            enqueue(sw->frame_t_price_queue, frame_t_price);
+            enqueue(sw->n_frame_item_queue, n_frame_item);
+
+            printf("%f\n", sw->window_t_price / sw->n_window_item);
+
+            for (long i = sw->prev_time + sw->hop_size; i < time; i += sw->hop_size) {
+                sw->window_t_price -= dequeue(sw->frame_t_price_queue);
+                sw->n_window_item -= dequeue(sw->n_frame_item_queue);
+                pushZero(sw, false);
+
+                printf("%f\n", sw->window_t_price / sw->n_window_item);
+
+                sw->cur_frame_starttime += sw->hop_size;
+            }
+
+            n_frame_item = 1;
+            frame_t_price = price;
+            sw->cur_frame_starttime += sw->hop_size;
+        }
+
+        sw->prev_time = time;
+    }
+}
+
+
 double compute_average(SlidingWindow *sw) {
     return sw->window_t_price / sw->n_window_item;
 }
+
+void pushZero(SlidingWindow *sw, bool count_queue) {
+    enqueue(sw->frame_t_price_queue, 0);
+    enqueue(sw->n_frame_item_queue, 0);
+    if (count_queue) {
+        queue_count++;
+    }
+}
+
+void ComplementZero(SlidingWindow *sw) {
+    for (long i = sw->cur_frame_starttime + sw->hop_size; i <= sw->deadline; i += sw->hop_size){
+        pushZero(sw, true);
+    }
+}
+
 
 int main() {
     char *file = "/Users/yunfanhu/Documents/AlgoTrading/hudsonthames/Implementations/FML-DataStructures/FML/processed-data/30-days/ETHUSDT-trades-2023-09.csv";
@@ -84,11 +153,6 @@ int main() {
 
     SlidingWindow *sw = sliding_window_create(24 * 60 * 60 * 1000, 1000);
 
-    char line[128];
-    char *token;
-    long time;
-    double price, volume;
-
     fgets(line, sizeof(line), fp);  // Skip header if any
     while (fgets(line, sizeof(line), fp)) {
         token = strtok(line, ",");
@@ -100,9 +164,13 @@ int main() {
         token = strtok(NULL, ",");
         volume = atof(token);
 
-        process_trade(sw, time, price, volume);
+        InitSlidingWindow(sw);
     }
-
+    ComplementZero(sw);
+    sw->cur_frame_starttime = time;
+    n_frame_item = 1;
+    frame_t_price = price;
+    SlideTheWindow(sw);
     printf("%f\n", compute_average(sw));
 
     sliding_window_destroy(sw);
